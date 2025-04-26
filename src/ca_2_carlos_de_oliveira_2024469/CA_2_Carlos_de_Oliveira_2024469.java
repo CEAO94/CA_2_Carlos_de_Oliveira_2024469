9af0521d179ae829557e307d23642cb2379ff6a5
Carlos Eduardo Alves de Oliveira <94c.oliveira@gmail.com>
2025-04-26 13:00:46 +0100

    CA2_2024469_Commit_01
    Initialize Java project structure for Tech Company organization (Algorithms & Constructors, Project Skills & Professionalism, Software Development Fundamentals assignment).

---
+++ src/ca_2_carlos_de_oliveira_2024469/CA_2_Carlos_de_Oliveira_2024469.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca_2_carlos_de_oliveira_2024469;
+
+/**
+ *
+ * @author carlo
+ */
+public class CA_2_Carlos_de_Oliveira_2024469 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
