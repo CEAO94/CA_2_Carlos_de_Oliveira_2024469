6d3d5b460cb42aa383f404670858172f2cb6e424
Carlos Eduardo Alves de Oliveira <94c.oliveira@gmail.com>
2025-04-26 14:21:54 +0100

    CA2_2024469_Commit_04
    Created an enum lists to further assign valid Departments (HR, IT and Customer.).

--- src/ca_2_carlos_de_oliveira_2024469/Department.java
+++ src/ca_2_carlos_de_oliveira_2024469/Department.java
@@ -6,8 +6,14 @@ package ca_2_carlos_de_oliveira_2024469;
 
 /**
  *
- * @author carlo
+ * @author Carlos Eduardo Alves de Oliveira - N.º 2024469
  */
-public class Department {
-    
+
+//Swap class for enum to creat a group of constants (final variables)
+//Further, this option will allow the assigment of valid Departments (HR, IT, Customer).
+public enum Department {
+    Human_Resources,
+    Technical_Support,
+    Customer_Service,
+            
 }
--- src/ca_2_carlos_de_oliveira_2024469/Function.java
+++ src/ca_2_carlos_de_oliveira_2024469/Function.java
@@ -6,8 +6,11 @@ package ca_2_carlos_de_oliveira_2024469;
 
 /**
  *
- * @author carlo
+ * @author Carlos Eduardo Alves de Oliveira | 2024469
  */
+
+//Swap class for enum to creat a group of constants (final variables)
+//Further, this option will allow the assigment of valid Job Roles.
 public enum Function {
     Director,
     Coordinator,
