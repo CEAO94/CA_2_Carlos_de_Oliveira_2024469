55d290f47cc92c185db07d5ed22612ceea140d5b
Carlos Eduardo Alves de Oliveira <94c.oliveira@gmail.com>
2025-04-26 14:46:19 +0100

    CA2_2024469_Commit_05
    Created a Class (String) to list further names, and return with valid Departments and Positions fixed with Enum.

--- src/ca_2_carlos_de_oliveira_2024469/Employee.java
+++ src/ca_2_carlos_de_oliveira_2024469/Employee.java
@@ -6,8 +6,38 @@ package ca_2_carlos_de_oliveira_2024469;
 
 /**
  *
- * @author carlo
+ * @author Carlos Eduardo Alves de Oliveira | 2024469
  */
+
+//This Java Class will define each employee attibute, such name, position and department
 public class Employee {
+    private String name;
+    private Function position;
+    private Department department;
     
+public Employee (String name, Function position, Department department) {
+    this.name = name;
+    this.position = position;
+    this.department = department;
+}
+
+//Will return a name from the list, as a String;
+public String getName() {
+    return name;
+}
+
+//Will return one of the valid Departments fixed as an ENUM in the parent Class Department;
+public Department getDepartment () {
+    return department;
+}
+
+//Will return one of the valid job roles fixed as an ENUM in the parent Class Function;
+public Function getPosition() {
+    return position;
+}
+
+//Will display the ramdom result, with "Name" follow by department and job role. 
+public String toString() {
+    return name + "|" + department + "|" + position + ";"; 
+}
 }
